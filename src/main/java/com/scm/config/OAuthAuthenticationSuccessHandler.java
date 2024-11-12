70e338f34960ee402678e8dbd99232536b3bb851
coderX4 <104690831+coderX4@users.noreply.github.com>
2024-11-12 22:42:57 +0530

    scm

--- src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
@@ -4,13 +4,13 @@ import com.scm.entity.Providers;
 import com.scm.entity.User;
 import com.scm.helper.AppConstants;
 import com.scm.repositories.UserRepo;
-import com.scm.services.impl.UserServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
@@ -24,7 +24,12 @@ import java.util.UUID;
 @Component
 public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    private User user;
+    private UserRepo userRepo;
+
+    @Autowired
+    public OAuthAuthenticationSuccessHandler(ApplicationContext context) {
+        this.userRepo = context.getBean(UserRepo.class);
+    }
 
     Logger logger = LoggerFactory.getLogger(OAuthAuthenticationSuccessHandler.class);
 
@@ -32,7 +37,7 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         logger.info("Authentication success");
 
-        DefaultOAuth2User oAuth2Useruser = (DefaultOAuth2User)authentication.getPrincipal();
+        DefaultOAuth2User oAuth2User = (DefaultOAuth2User)authentication.getPrincipal();
 
 //        logger.info("oAuth2Useruser: {}", oAuth2Useruser.getName());
 //        oAuth2Useruser.getAttributes().forEach((key, value) -> {
@@ -40,59 +45,35 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
 //        });
 //        logger.info(oAuth2Useruser.getAuthorities().toString());
 
-        String Email = oAuth2Useruser.getAttribute("email");
-        //logger.info("Email: " + Email);
-        String userName = oAuth2Useruser.getAttribute("name");
-        //logger.info("userName: " + userName);
-        String picture = oAuth2Useruser.getAttribute("picture");
-        //logger.info("picture: " + picture);
+        String Email = oAuth2User.getAttribute("email");
+        logger.info("Email: " + Email);
+        String userName = oAuth2User.getAttribute("name");
+        logger.info("userName: " + userName);
+        String picture = oAuth2User.getAttribute("picture");
+        logger.info("picture: " + picture);
 
         //create user and save in database
-//        user.setEmail(Email);
-//        user.setUserName(userName);
-//        user.setProfilePic(picture);
-//        user.setPassword("SCM@123");
-//        user.setUserId(UUID.randomUUID().toString());
-//        user.setProvider(Providers.GOOGLE);
-//        user.setEnabled(true);
-//        user.setEmailVerified(true);
-//        user.setProviderUserId(oAuth2Useruser.getName());
-//        user.setRolesList(List.of(AppConstants.ROLE_USER));
-//        user.setAbout("This Account is created by google login");
-//
-//        sendUser(Email);
-//        User user1 = userRepo.findByEmail(Email).orElse(null);
-//        if (user1 == null) {
-//            userRepo.save(user);
-//            logger.info("User created" + Email);
-//        }
-        new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
-
-        user = setGoogleUser(Email, userName, picture);
-
-        logger.info("email: " + user.getEmail());
-        logger.info("name: " + user.getUsername());
-        logger.info("picture: " + user.getProfilePic());
-        logger.info("Provider: " + user.getProvider());
-
-    }
-
-    public User setGoogleUser(String email, String name, String picture) {
-
         User user = new User();
-        user.setEmail(email);
-        user.setUserName(name);
+        user.setEmail(Email);
+        user.setUserName(userName);
         user.setProfilePic(picture);
         user.setPassword("SCM@123");
         user.setUserId(UUID.randomUUID().toString());
         user.setProvider(Providers.GOOGLE);
         user.setEnabled(true);
         user.setEmailVerified(true);
+        user.setProviderUserId(oAuth2User.getName());
         user.setRolesList(List.of(AppConstants.ROLE_USER));
         user.setAbout("This Account is created by google login");
 
-        return user;
+
+        User user1 = userRepo.findByEmail(Email).orElse(null);
+        if (user1 == null) {
+            userRepo.save(user);
+            logger.info("User created" + Email);
+        }
+        new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
+
     }
-    
 
 }
--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,27 +1,23 @@
 package com.scm.config;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
 
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private OAuthAuthenticationSuccessHandler oAuthAuthenticationSuccessHandler;
     //user create and login using java code with in memory service
     //this method is only for testing
 //    @Bean
@@ -94,7 +90,7 @@ public class SecurityConfig {
         //oauth2 configurations
         httpSecurity.oauth2Login(oauth2Login->{
             oauth2Login.loginPage("/login");
-            oauth2Login.successHandler(new OAuthAuthenticationSuccessHandler());
+            oauth2Login.successHandler(oAuthAuthenticationSuccessHandler);
         });
 
         return httpSecurity.build();
