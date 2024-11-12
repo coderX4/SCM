75a3b2c9ea4ab4dbaf8348b88bbe49e26f159d61
coderX4 <104690831+coderX4@users.noreply.github.com>
2024-11-12 12:04:56 +0530

    scm

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.scm.config;
 
 import com.scm.services.impl.SecurityCustomUserDetailService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 
 @Configuration
@@ -81,6 +89,12 @@ public class SecurityConfig {
         httpSecurity.logout(logout->{
             logout.logoutUrl("/user/do-logout");
             logout.logoutSuccessUrl("/login?logout=true");
+        });
+
+        //oauth2 configurations
+        httpSecurity.oauth2Login(oauth2Login->{
+            oauth2Login.loginPage("/login");
+            oauth2Login.successHandler(new OAuthAuthenticationSuccessHandler());
         });
 
         return httpSecurity.build();
