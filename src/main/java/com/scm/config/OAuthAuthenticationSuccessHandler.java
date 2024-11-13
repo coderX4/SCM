7abcf9af0e39023756a51c81e010ef42a5ec6be0
coderX4 <104690831+coderX4@users.noreply.github.com>
2024-11-13 09:56:40 +0530

    scm

--- src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -36,6 +37,54 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         logger.info("Authentication success");
+
+        var oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+        String authorizedClientRegistrationId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+
+        logger.info(authorizedClientRegistrationId);
+
+        var oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+        oauthUser.getAttributes().forEach((key, value) -> {
+            logger.info(key + " : " + value);
+        });
+
+        User user = new User();
+        user.setUserId(UUID.randomUUID().toString());
+        user.setRolesList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerified(true);
+        user.setEnabled(true);
+        user.setPassword("scm@123");
+
+        if(authorizedClientRegistrationId.equalsIgnoreCase("google")){
+            user.setEmail(oauthUser.getAttribute("email").toString());
+            user.setProfilePic(oauthUser.getAttribute("picture").toString());
+            user.setUserName(oauthUser.getAttribute("name").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setProvider(Providers.GOOGLE);
+            user.setAbout("This account is created using google.");
+        }
+        else if(authorizedClientRegistrationId.equalsIgnoreCase("github")){
+            // github
+            // github attributes
+            String email = oauthUser.getAttribute("email") != null ? oauthUser.getAttribute("email").toString()
+                    : oauthUser.getAttribute("login").toString() + "@gmail.com";
+            String picture = oauthUser.getAttribute("avatar_url").toString();
+            String name = oauthUser.getAttribute("login").toString();
+            String providerUserId = oauthUser.getName();
+
+            user.setEmail(email);
+            user.setProfilePic(picture);
+            user.setUserName(name);
+            user.setProviderUserId(providerUserId);
+            user.setProvider(Providers.GITHUB);
+
+            user.setAbout("This account is created using github");
+        }
+        else{
+            logger.info("Oauthorization through: Unknown Provider");
+        }
+
+        /*
 
         DefaultOAuth2User oAuth2User = (DefaultOAuth2User)authentication.getPrincipal();
 
@@ -65,12 +114,11 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
         user.setProviderUserId(oAuth2User.getName());
         user.setRolesList(List.of(AppConstants.ROLE_USER));
         user.setAbout("This Account is created by google login");
-
-
-        User user1 = userRepo.findByEmail(Email).orElse(null);
+*/
+        User user1 = userRepo.findByEmail(user.getEmail()).orElse(null);
         if (user1 == null) {
             userRepo.save(user);
-            logger.info("User created" + Email);
+            logger.info("User created" + user.getEmail());
         }
         new DefaultRedirectStrategy().sendRedirect(request, response, "/user/dashboard");
 
